50d002c4d28961b40a469ffa7877896de76a658a
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2025-06-18 19:21:11 -0300

    feat: implements home screen with Pokémon list (name + image)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from '../services/pokeapi.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: '/home.page.html',
+  styleUrls: ['/home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  pokemons: any[] = [];
 
+  constructor(
+    private pokeService: PokeapiService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    this.pokeService.getPokemons().subscribe(async (res: any) => {
+      const requests = res.results.map((pokemon: any) => this.pokeService.getPokemonDetails(pokemon.url));
+      const details = await Promise.all(requests.map((req: { toPromise: () => any; }) => req.toPromise()));
+      this.pokemons = details;
+    });
+  }
+
+  goToDetail(name: string) {
+    this.router.navigate(['/pokemon-detail', name]);
+  }
 }
